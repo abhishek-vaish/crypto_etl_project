65ae13f0d96cfc148063bb8f1eaba6dca28ea4c2
abhishek-vaish <abhishekv5055@gmail.com>
2024-06-04 16:53:04 +0530

    clean coinmarketcap scripts

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from pathlib import Path
-from datetime import datetime
 import os
 
 from dotenv import load_dotenv
 
-from src.scrapping.scrap_coinmarketcap import CoinMarketCap
 from src.scrapping.scrap_cryptoranking import CryptoRanking
 from src.etl.extract_coinranking import ExtractCoinRanking
-from src.etl.transformation import Transformation
 from src.etl.load import Load
-from src.table.CoinMarketCap import BaseCoinMarketCap
+from src.table.CoinRanking import BaseCoinMarketCap
 
 
 if __name__ == '__main__':
@@ -21,11 +18,6 @@ if __name__ == '__main__':
     if not ETL_PATH.is_dir() or not ARCHIVE_PATH.is_dir():
         ETL_PATH.mkdir(exist_ok=True)
         ARCHIVE_PATH.mkdir(exist_ok=True)
-
-    # scrap data from the target link
-    # coinmarketcap_obj = CoinMarketCap()
-    # coinmarketcap_lst = coinmarketcap_obj.scrap_data()
-    # coinmarketcap_obj.convert_dict_to_csv(coinmarketcap_lst, ETL_PATH)
 
     # scrap data from coin ranking API
     API_KEY = os.getenv("API_KEY")
@@ -43,24 +35,6 @@ if __name__ == '__main__':
         .read_file("cryptoranking_*") \
         .get_dictonary()
 
-    # process files
-    # for file in ETL_PATH.glob("coinmarketcap_*.csv"):
-    #     if file:
-    #         # create file date
-    #         file_datetime = str(file).split("_")[1] + " " + str(file).split("_")[2][:-4]
-    #         file_datetime = datetime.strptime(file_datetime, '%Y%m%d %H%M')
-    #
-    #         # perform transformation
-    #         transformation = Transformation(file_path=file) \
-    #             .file_format("csv") \
-    #             .read_file() \
-    #             .remove_null() \
-    #             .remove_duplicate() \
-    #             .split_substring("market_cap", "\\$[0-9]+\\.?[0-9]*[A-Z]") \
-    #             .cast("rank", int) \
-    #             .generate_dict()
-    #
-    #         # load data to sql table
     Load(data_list, SERVER_NAME) \
         .connect_engine() \
         .insert(BaseCoinMarketCap) \
