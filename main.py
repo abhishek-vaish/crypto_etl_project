c66159d62414e32795be32793a1cea39f56874bc
abhishek-vaish <abhishekv5055@gmail.com>
2024-06-04 16:44:36 +0530

    add coinranking api

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import os
 from dotenv import load_dotenv
 
 from src.scrapping.scrap_coinmarketcap import CoinMarketCap
+from src.scrapping.scrap_cryptoranking import CryptoRanking
+from src.etl.extract_coinranking import ExtractCoinRanking
 from src.etl.transformation import Transformation
 from src.etl.load import Load
 from src.table.CoinMarketCap import BaseCoinMarketCap
-from src.utilities.utility import archive_file
 
 
 if __name__ == '__main__':
@@ -22,38 +23,45 @@ if __name__ == '__main__':
         ARCHIVE_PATH.mkdir(exist_ok=True)
 
     # scrap data from the target link
-    coinmarketcap_obj = CoinMarketCap()
-    coinmarketcap_lst = coinmarketcap_obj.scrap_data()
-    coinmarketcap_obj.convert_dict_to_csv(coinmarketcap_lst, ETL_PATH)
+    # coinmarketcap_obj = CoinMarketCap()
+    # coinmarketcap_lst = coinmarketcap_obj.scrap_data()
+    # coinmarketcap_obj.convert_dict_to_csv(coinmarketcap_lst, ETL_PATH)
+
+    # scrap data from coin ranking API
+    API_KEY = os.getenv("API_KEY")
+    COIN_RANKING_URI = os.getenv("CRYPTO_RANKING_URI")
+    COIN_RANKING_LIMIT = os.getenv("API_LIMIT")
+    COIN_RANKING_VERSION = os.getenv("CRYPTO_RANKING_VERSION")
+    coinranking_obj = CryptoRanking(API_KEY,
+                                    COIN_RANKING_URI,
+                                    COIN_RANKING_LIMIT)
+    coinranking_obj.req_crypto_ranking("currencies") \
+        .save_to_json(ETL_PATH)
+
+    data_list = ExtractCoinRanking(ETL_PATH) \
+        .format("json") \
+        .read_file("cryptoranking_*") \
+        .get_dictonary()
 
     # process files
-    for file in ETL_PATH.glob("coinmarketcap_*.csv"):
-        if file:
-            # create file date
-            file_datetime = str(file).split("_")[1] + " " + str(file).split("_")[2][:-4]
-            file_datetime = datetime.strptime(file_datetime, '%Y%m%d %H%M')
-
-            # perform transformation
-            transformation = Transformation(file_path=file) \
-                .file_format("csv") \
-                .read_file() \
-                .remove_null() \
-                .remove_duplicate() \
-                .split_substring("market_cap", "\\$[0-9]+\\.?[0-9]*[A-Z]") \
-                .cast("rank", int) \
-                .generate_dict()
-
-            # load data to sql table
-            Load(transformation, SERVER_NAME) \
-                .connect_engine() \
-                .get_key("coinmarketcap", "raw")\
-                .insert(BaseCoinMarketCap, file_datetime) \
-                .close()
-
-            # archive process files
-            file_string = os.path.split(file)[1]
-            arhive_file_path = ARCHIVE_PATH / file_string
-            archive_file(file, arhive_file_path)
-        else:
-            raise FileNotFoundError(f"No files found at: {ETL_PATH}")
-
+    # for file in ETL_PATH.glob("coinmarketcap_*.csv"):
+    #     if file:
+    #         # create file date
+    #         file_datetime = str(file).split("_")[1] + " " + str(file).split("_")[2][:-4]
+    #         file_datetime = datetime.strptime(file_datetime, '%Y%m%d %H%M')
+    #
+    #         # perform transformation
+    #         transformation = Transformation(file_path=file) \
+    #             .file_format("csv") \
+    #             .read_file() \
+    #             .remove_null() \
+    #             .remove_duplicate() \
+    #             .split_substring("market_cap", "\\$[0-9]+\\.?[0-9]*[A-Z]") \
+    #             .cast("rank", int) \
+    #             .generate_dict()
+    #
+    #         # load data to sql table
+    Load(data_list, SERVER_NAME) \
+        .connect_engine() \
+        .insert(BaseCoinMarketCap) \
+        .close()
--- src/etl/load.py
+++ src/etl/load.py
@@ -23,25 +23,10 @@ class Load:
         self.key = key if key is not None else 0
         return self
 
-    def insert(self, table_name, file_date):
-        for data in self.df:
-            rows = [{
-                "Id": self.key + 1,
-                "Rank": data.get("rank"),
-                "Name": data.get("name"),
-                "Symbol": data.get("symbol"),
-                "MarketCap": data.get("market_cap"),
-                "Price": data.get("price"),
-                "CirculatingSupply": data.get("circulating_supply"),
-                "Volume": data.get("volume"),
-                "PercentPerHour": data.get("percent_per_hour"),
-                "PercentPerWeek": data.get("percent_per_week"),
-                "PercentPerDay": data.get("percent_per_day"),
-                "FileDate": file_date
-            }]
-            self.cursor.execute(table_name.__table__.insert().values(rows))
-            self.key += 1
-        self.cursor.commit()
+    def insert(self, table_name):
+        for row in self.df:
+            self.cursor.execute(table_name.__table__.insert().values(row))
+            self.cursor.commit()
         return self
 
     def close(self):
